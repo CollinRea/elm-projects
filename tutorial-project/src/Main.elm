a66722bb805e133d3af541d8f76478e215a138bc
Collin Rea <collinr01@gmail.com>
2016-10-17 07:20:46 -0500

    Completed tutorial project, also edited first-project but will take that back to being portfolio app once I have some more time to dig into elm

--- tutorial-project/src/Main.elm
+++ tutorial-project/src/Main.elm
@@ -1,16 +1,21 @@
 module Main exposing (..)
 
-import Html.App as App
+import Navigation
 import Messages exposing (Msg(..))
 import Models exposing (Model, initialModel)
 import View exposing (view)
 import Update exposing (update)
 import Players.Commands exposing (fetchAll)
+import Routing exposing (Route)
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( initialModel, Cmd.map PlayersMsg fetchAll )
+init : Result String Route -> ( Model, Cmd Msg )
+init result =
+    let
+        currentRoute =
+            Routing.routeFromResult result
+    in
+        ( initialModel currentRoute, Cmd.map PlayersMsg fetchAll )
 
 
 
@@ -22,15 +27,25 @@ subscriptions model =
     Sub.none
 
 
+urlUpdate : Result String Route -> Model -> ( Model, Cmd Msg )
+urlUpdate result model =
+    let
+        currentRoute =
+            Routing.routeFromResult result
+    in
+        ( { model | route = currentRoute }, Cmd.none )
+
+
 
 -- MAIN
 
 
 main : Program Never
 main =
-    App.program
+    Navigation.program Routing.parser
         { init = init
         , view = view
         , update = update
+        , urlUpdate = urlUpdate
         , subscriptions = subscriptions
         }
